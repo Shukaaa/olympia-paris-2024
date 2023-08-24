0372634e6ce7e4852b1a08d61a99536c65a69da3
Shukaaa <asappokimane@gmail.com>
2023-08-24 11:31:07 +0200

    initialized java spring boot

---
+++ service/src/main/java/cum/ass/olympiaparisservice/OlympiaParisServiceApplication.java
@@ -0,0 +1,13 @@
+package cum.ass.olympiaparisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympiaParisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympiaParisServiceApplication.class, args);
+	}
+
+}
